6bcba895e833e262b7f2a4627780278857fe111a
Garrett <stmichaelboy@icloud.com>
2025-12-20 12:59:53 -0500

    Update wordcounter.py

--- wordcounter.py
+++ wordcounter.py
@@ -1,5 +1,6 @@
 #wordcounter
 
+##example speech, this should work for every body of text
 speech = """
 The computer is something extraordinary.
 It's going to probably take over our lives.
@@ -15,15 +16,20 @@ Or, be thoroughly employed in amusement ... in entertainment?
 Please face all this, for God's sake, this is happening.
 """
 
+##splits up text into a list containing each word
 word_list = speech.split
+##prints how many words there are in total
 print(len(word_list()))
 
+#creates empty list to fill
 number_of_words = {}
 
+##for loop that will go through each word to see how many times it is in the text
 for word in word_list():
     if word in number_of_words:
         number_of_words[word] += 1
     else:
         number_of_words[word]=1
+
 
 print(number_of_words)
