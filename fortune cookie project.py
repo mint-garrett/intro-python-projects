b7c1714a964dbff9937d31a6bd7fab3f923f846d
Garrett <stmichaelboy@icloud.com>
2025-12-20 12:51:55 -0500

    Update fortune cookie project.py

--- fortune cookie project.py
+++ fortune cookie project.py
@@ -1,10 +1,12 @@
 ##fortune_cookie.py
 
 import random;
+##rand int for lucky number
 lucky_number = random.randint(1,50);
-
+### rand int between 1-5 for phrase assignment
 sentence_number= random.randint(1,5)
 
+##5 phrases that sound like they would come from a fortune cookie
 if sentence_number == 1:
     printed_phrase= "Bamboo sprouts so fast and so tall, only to fall to the humble panda."
 
@@ -22,4 +24,5 @@ if sentence_number == 5:
 
 print(f"{printed_phrase}");
 print(f"Your lucky number on this fine day is: {lucky_number}");
+
 
